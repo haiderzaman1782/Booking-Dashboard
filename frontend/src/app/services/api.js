3a6582a8d96169342dac6d3e24973e122fbdef77
haider zaman <alihaiderop078@outlook.com>
2025-12-24 16:41:10 +0500

    Update api.js

--- frontend/src/app/services/api.js
+++ frontend/src/app/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = import.meta.env.VITE_API_URL;
+const API_BASE_URL = import.meta.env.VITE_API_URL || 'https://booking-dashboard-7gg6.vercel.app/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
